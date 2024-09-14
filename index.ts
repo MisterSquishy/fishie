2dfabfb8ef9be416af271d7773501f4b34abf1cc
MisterSquishy <pdavids@comcast.net>
2024-09-13 22:12:38 -0400

    teehee

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import 'dotenv';
 import FormData from "form-data";
-import { createReadStream, writeFileSync } from 'fs';
+import { createReadStream } from 'fs';
+import { IgApiClient } from 'instagram-private-api';
+import pngToJpeg from 'png-to-jpeg';
 import { env } from 'process';
 
 const DEZGO_API_URL = 'https://api.dezgo.com/text-inpainting'
@@ -12,12 +14,25 @@ module.exports.handler = async (event) => {
   formData.append('prompt', 'fish');
   formData.append('init_image', createReadStream("bird.jpeg"))
 
-  const result = await axios.postForm(DEZGO_API_URL, formData, {
+  const response = await axios.postForm(DEZGO_API_URL, formData, {
     headers: {
       "X-Dezgo-Key": env['X-Dezgo-Key'],
     },
     timeout: 60000,
     responseType: 'arraybuffer',
   })
-  writeFileSync("fish.png", result.data);
+
+  const jpg = await pngToJpeg({ quality: 100 })(response.data)
+  await postToInsta(jpg, 'test')
 };
+
+const postToInsta = async (image: Buffer, caption: string) => {
+  const ig = new IgApiClient();
+  ig.state.generateDevice(env.IG_USERNAME!);
+  await ig.account.login(env.IG_USERNAME!, env.IG_PASSWORD!);
+  await ig.publish.photo({
+    file: image,
+    caption,
+  });
+}
+
