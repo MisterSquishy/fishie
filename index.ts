21bbba550ccdc87cd4370219950bffd8d02b944e
MisterSquishy <pdavids@comcast.net>
2024-09-13 21:54:16 -0400

    ok wait this is fucking sick

--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
+import axios from 'axios';
 import 'dotenv';
-import fs from "fs";
-import OpenAI from "openai";
+import FormData from "form-data";
+import { createReadStream, writeFileSync } from 'fs';
+import { env } from 'process';
+
+const DEZGO_API_URL = 'https://api.dezgo.com/text-inpainting'
 
 module.exports.handler = async (event) => {
-  const openai = new OpenAI();
+  const formData = new FormData();
+  formData.append('mask_prompt', 'bird');
+  formData.append('prompt', 'fish');
+  formData.append('init_image', createReadStream("bird.jpeg"))
 
-  async function main() {
-    const image = await openai.images.edit({
-      image: fs.createReadStream("bird.png"),
-      prompt: "Replace the bird in this image with a fish",
-    });
-
-    console.log(image.data);
-  }
-  main();
+  const result = await axios.postForm(DEZGO_API_URL, formData, {
+    headers: {
+      "X-Dezgo-Key": env['X-Dezgo-Key'],
+    },
+    timeout: 60000,
+    responseType: 'arraybuffer',
+  })
+  writeFileSync("fish.png", result.data);
 };
